bd5c67df1b62e3a7d85ed34e0124f976e5899660
Fabriexe <fabricioexe54@gmail.com>
2021-09-16 23:03:54 -0300

    Update

--- TP5-Busquedas Locales/CODE/test.py
+++ TP5-Busquedas Locales/CODE/test.py
@@ -8,8 +8,8 @@ print("")
 Hc=0
 timeHistory=[]
 Hcstat=[]
-queens=8
-inte=60
+queens=12
+inte=30
 results=[]
 
 for i in range(0,inte):
@@ -39,5 +39,40 @@ if len(Hcstat)>=2:
     print("Porcentaje de soluciones Optimas",(Hc/inte)*100)
     print("Media de estados por resolucion",mean(Hcstat))
     print("Desviación estandar de estados por resolución",stdev(Hcstat))
+else:
+    print("No se encontraron soluciones")
+
+print("XXX")
+print("Resultados Simulated")
+SA=0
+timeHistory=[]
+SAstates=[]
+for i in range(0,inte):
+    br=board(queens)
+    results=[]
+    start=time.time()
+    for j in range(0,100):
+        val=br.annealing()
+        results.append(val)
+        if val==0:
+            SAstates.append(i)
+            break
+    last=results.pop()
+    end=time.time()
+    if last==0:
+        SA+=1
+        timeHistory.append(end-start)
+        results.append(last)
+        plt.plot(results)
+        plt.title("variacion de la funcion SA")
+        plt.show
+
+if len(SAstates)>=2:
+    print(SA)
+    print("Media tiempo de resolución",mean(timeHistory))
+    print("Desviacion estandar de tiempo de resolución",stdev(timeHistory))
+    print("Porcentaje de soluciones Optimas",(SA/inte)*100)
+    print("Media de estados por resolucion",mean(SAstates))
+    print("Desviación estandar de estados por resolución",stdev(SAstates))
 else:
     print("No se encontraron soluciones")
